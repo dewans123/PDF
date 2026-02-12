bb91b1df953ce779a4051e13060a44bbd2cda57d
dewans123 <dewans97373@gmail.com>
2026-02-12 09:24:07 +0530

    Delete Array2d.java

--- Array2d.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-class Array2d{
-	public static void main(String[] SK){
-		String[][] cities = {{"Delhi","Mumbai"},{"Kolkatta","Chennai"}};
-		System.out.println(Arrays.deepToString(cities));
-	}
-}
