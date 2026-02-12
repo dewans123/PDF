92a301d3823f25be8bff6721c9fd69c9795d5572
dewans123 <dewans97373@gmail.com>
2026-02-12 09:23:05 +0530

    Delete Array1d.java

--- Array1d.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-class Array1d{
-	public static void main(String[] SK){
-		String[] cities = {"Mumbai","Kolkatta","Delhi","Ahemdabad","Chennai"};
-		System.out.println(Arrays.toString(cities));
-	}
-}
